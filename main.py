26a1634b85a33936ef1612828299576a2fb20911
Jibin Mathew <jibintmathew1997@gmail.com>
2022-10-12 17:29:01 +1100

    small tests and checks

--- main.py
+++ main.py
@@ -215,7 +215,7 @@ def main():
     #pull from database
     df = pd.read_csv(database_path)
     try: 
-        #url = f'https://docs.google.com/spreadsheets/d/1P3Qu1EQLgcQYWSZQwjY5OWmEnnJMvSSgLkasa6rMC6E/gviz/tq?tqx=out:csv'
+        url = f'https://docs.google.com/spreadsheets/d/1P3Qu1EQLgcQYWSZQwjY5OWmEnnJMvSSgLkasa6rMC6E/gviz/tq?tqx=out:csv'
         df = pd.read_csv(url)
     except:
         df = pd.read_csv(database_path)
